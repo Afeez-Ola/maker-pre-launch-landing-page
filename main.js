9fec9ee53d8e2cbb5d36135d82c2b7c657a3ca41
Afeez Bolaji <afeezbolajiola@gmail.com>
2025-07-25 06:44:19 +0200

    Adding javascript functionality to the app.

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
 const Form = document.getElementById("form");
-const email = document.getElementById("email");
+const email = document.getElementById("email").value;
 const submitButton = document.getElementById("submitButton");
+const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const errorMessage = document.getElementById("errorMessage")
+
+Form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  validateEmail()
+})
+
+function validateEmail(){
+  if (!emailPattern.test(email)) {
+    errorMessage.classList.remove("hidden")
+    return false
+  }
+
+  errorMessage.classList.add("hidden")
+  return true
+}
 
