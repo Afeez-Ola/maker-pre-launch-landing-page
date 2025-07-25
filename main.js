ab63bbe11ed5a626a0184bcb6b89ac96a2884be6
Afeez Bolaji <afeezbolajiola@gmail.com>
2025-07-25 06:45:48 +0200

    Having fun haha

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
 const Form = document.getElementById("form");
-const email = document.getElementById("email").value;
 const submitButton = document.getElementById("submitButton");
 const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 const errorMessage = document.getElementById("errorMessage")
 
 Form.addEventListener("submit", (e) => {
+  if (!validateEmail) {
   e.preventDefault();
-  validateEmail()
+  }
 })
-
 function validateEmail(){
+  const email = document.getElementById("email").value;
   if (!emailPattern.test(email)) {
     errorMessage.classList.remove("hidden")
     return false
   }
-
   errorMessage.classList.add("hidden")
   return true
 }
